0a4799afc32647f08461478e930ea40da29c03ca
ziidd12 <triomadkar11@gmail.com>
2026-02-05 14:28:03 +0700

    tambahin_beberapa fitur

--- app/Http/Controllers/tahunAjaranController.php
+++ app/Http/Controllers/tahunAjaranController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AbsensiController extends Controller
+class tahunAjaranController extends Controller
 {
-    
+    //
 }
--- app/Models/Guru.php
+++ app/Models/Guru.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Guru extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'guru';
+
+    protected $fillable = [
+        'user_id',
+        'nama_guru',
+        'NIP',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
